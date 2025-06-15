640d9fa38804fa875d5037d7032bed3abaa919f6
abdallahali5 <abdallahali5.aaa@gmail.com>
2025-06-15 16:24:20 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', function(event) {
+  console.log('Service Worker Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
